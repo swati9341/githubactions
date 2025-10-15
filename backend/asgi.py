2b4bacb1d003aa781cac1aec4251cfd99bdd85b7
swati9341 <swati30092004@gmail.com>
2025-10-15 20:33:49 +0530

    checking connnection

--- backend/asgi.py
+++ backend/asgi.py
@@ -2,7 +2,7 @@
 ASGI config for backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
-
+swati singh
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
